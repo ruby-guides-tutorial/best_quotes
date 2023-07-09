4af0db22342b7d1bafa64460639a5259672e5991
guxiaobai <sikuan.gu@gmail.com>
2023-07-09 11:14:59 +0800

    2. Your First Controller

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,8 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'app', 'controllers')
+
 require 'rulers'
+require "quotes_controller"
+
 
 module BestQuotes
   class Application < Rulers::Application
