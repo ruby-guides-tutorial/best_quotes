f38476c344138a193cf3a003830b6a2512254f4f
guxiaobai <sikuan.gu@gmail.com>
2023-07-10 00:58:42 +0800

    4. Rendering Views

---
+++ erb_test.rb
@@ -0,0 +1,12 @@
+require "erubis"
+
+
+template = <<TEMPLATE
+Hello! This is a template.
+It has <%= whatever %>.
+TEMPLATE
+
+eruby = Erubis::Eruby.new(template)
+puts eruby.src
+puts "======="
+puts eruby.result(:whatever=> "ponies")
